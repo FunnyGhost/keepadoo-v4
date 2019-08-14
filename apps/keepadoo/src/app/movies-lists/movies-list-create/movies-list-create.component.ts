41130aef0a4b762d34c0d1faf98799126ba80110
Catalin Ciubotaru <catalinciubot@gmail.com>
2019-08-14 09:36:42 +0200

    Implemented the MoviesListCreate component

--- apps/keepadoo/src/app/movies-lists/movies-list-create/movies-list-create.component.ts
+++ apps/keepadoo/src/app/movies-lists/movies-list-create/movies-list-create.component.ts
@@ -1,12 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MoviesListsQuery } from '../state/movies-lists.query';
+import { MoviesListsService } from '../state/movies-lists.service';
 
 @Component({
   selector: 'keepadoo-movies-list-create',
   templateUrl: './movies-list-create.component.html',
-  styleUrls: ['./movies-list-create.component.scss']
+  styleUrls: ['./movies-list-create.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MoviesListCreateComponent implements OnInit {
-  constructor() {}
+  moviesListForm = this.formBuilder.group({
+    name: ['', [Validators.minLength(2), Validators.required]]
+  });
 
-  ngOnInit() {}
+  error$: Observable<string>;
+  loading$: Observable<boolean>;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private query: MoviesListsQuery,
+    private moviesListService: MoviesListsService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.error$ = this.query.selectError();
+    this.loading$ = this.query.selectLoading();
+  }
+
+  async onSubmit() {
+    await this.moviesListService.add({ name: this.moviesListForm.value.name });
+    this.goBack();
+  }
+
+  goBack(): void {
+    this.router.navigate(['../'], { relativeTo: this.activatedRoute });
+  }
 }
